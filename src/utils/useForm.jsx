5fdaa9ad8a0fda761023697ed7a18ac2575b09fe
Farid Amimri <farid.amimri@gmail.com>
2022-08-21 22:19:32 +0200

    Seperate common course with useForm

---
+++ src/utils/useForm.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import { React, useState } from 'react'
+
+export function useForm(initialFValues) {
+  const [values, setValues] = useState(initialFValues)
+  const [errors, setErrors] = useState({})
+
+  return {
+    values,
+    setValues,
+    errors,
+    setErrors
+  }
+}
