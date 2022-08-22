9c9ef3a8663b25a2a885882d91e2927e9ed931e6
Farid Amimri <farid.amimri@gmail.com>
2022-08-22 22:58:31 +0200

    Replace with components and adding state records

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 /** @format */
 
-import React from 'react'
-import BackCard from './assets/back-card.png'
-import MuiForm from './components/MuiForm'
+import {React, useState} from 'react'
+import FrontCard from './components/FrontCard'
+import BackCard from './components/BackCard'
+import Form from './components/Form'
 
 function App() {
+
+  const [records, setRecords] = useState([])
+
   return (
     <div className='App'>
       <div className='layout'></div>
-
-      <div className='face-card-details'>
-        <div className='card-header'>
-          <div className='big-circle'></div>
-          <div className='small-circle'></div>
-        </div>
-        <p className='card-number'>0000 0000 0000 0000</p>
-        <div className='card-footer'>
-          <p className='card-name'>JANE APPLESEED</p>
-          <p className='card-name'>00/00</p>
-        </div>
-        <div className='oval-container'>
-          <div className='oval-pink'></div>
-          <div className='oval-orange'></div>
-          <div className='oval-blue'></div>
-        </div>
-      </div>
-
-      <div className='back-card-details'>
-        <div className='black-rectangle'></div>
-        <div className='grey-rectangle'></div>
-        <img src={BackCard} alt='details' className='backcard-png'></img>
-      </div>
-
-      <MuiForm />
+      <FrontCard records={records} />
+      <BackCard records={records} />
+      <Form setRecords={setRecords} />
     </div>
   )
 }
